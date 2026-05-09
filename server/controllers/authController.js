1bd34829c5bde41c5b6ab692d9010f8f9bed9bc9
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-05-09 11:01:27 +0530

    API Error
    day 4. 9/5/26  facing error to connect API properly

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -90,3 +90,19 @@ export const login = async (req, res)=>{
     }
 
 }
+
+
+
+export const logout = async ()=>{
+    try {
+        res.clearCookie('token', {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === 'production',
+            sameSite: process.env.NODE_ENV === 'production' ? 'none' : 'strict',
+        })
+        return res.json({success: true, message: "Logged Out Successfully"})
+
+    } catch (error){
+        return res.json({ success: false, message: error.message });
+    }
+}
