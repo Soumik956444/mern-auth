9d5407e937958b4a63faf73c8d59a65e9c44dcec
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-05-10 13:02:23 +0530

    API Connection
    Established API connection using POSTMAN

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ import 'dotenv/config';
 import cookieParser from "cookie-parser";
 
 import connectDB from './config/mongodb.js';
-import authRouter from './routes/authRoutes.js'
+import { authRouter } from './routes/authRoutes.js';
 
 const app = express();
 const port = process.env.PORT || 4000
@@ -17,7 +17,7 @@ app.use(cors({credentials: true}))
 // API Routes
 // API Endpoints 
 app.get('/', (req, res)=> res.send("API Working"))
-app.use('/api/auth', authRouter)
+app.use('/api/auth', authRouter);
 
 
 app.listen(port, ()=> console.log(`Server started on PORT:${port}`));
