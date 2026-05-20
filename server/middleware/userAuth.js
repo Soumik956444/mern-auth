d38ebb217754b6aa4e07f6e36547ff5cbfd17fb3
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-05-20 21:24:50 +0530

    Update userAuth.js

--- server/middleware/userAuth.js
+++ server/middleware/userAuth.js
@@ -1,76 +1,40 @@
-// import jwt from "jsonwebtoken";
-
-// const userAuth = async (req, res, next) => {
-//     const token = req.cookies;
-
-//     if (!token){
-//         return res.json({success: false, message: 'Not Authorized. Login Again'})
-//     }
-
-//     try {
-
-//         const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET)
-
-//         if(tokenDecode.id){
-//             req.body.userId = tokenDecode.id
-//         }else{
-//             return res.json({success: false, message: 'Not Authorized. Login Again'});
-//         }
-
-//         next();
-
-
-//     } catch (error) {
-//         res.json({success: false, message: error.message});
-//     }
-
-// }
-
-// export default userAuth;
-
-
-
-
-
-
-
-
-
-
-
-
-
-import jwt from 'jsonwebtoken';
+import jwt from "jsonwebtoken";
 
 const userAuth = async (req, res, next) => {
+    const token = req.cookies;
+
+    if (!token){
+        return res.json({success: false, message: 'Not Authorized. Login Again'})
+    }
+
     try {
 
-        const authHeader = req.headers.authorization;
+        const tokenDecoded = jwt.verify(token, process.env.JWT_SECRET)
 
-        if (!authHeader || !authHeader.startsWith('Bearer ')) {
-            return res.json({
-                success: false,
-                message: 'Token Missing'
-            });
+        if(tokenDecode.id){
+            req.body.userId = tokenDecode.id
+        }else{
+            return res.json({success: false, message: 'Not Authorized. Login Again'});
         }
-
-        const token = authHeader.split(' ')[1];
-
-        const decoded = jwt.verify(
-            token,
-            process.env.JWT_SECRET
-        );
-
-        req.userId = decoded.id;
 
         next();
 
+
     } catch (error) {
-        return res.json({
-            success: false,
-            message: error.message
-        });
+        res.json({success: false, message: error.message});
     }
-};
+
+}
 
 export default userAuth;
+
+
+
+
+
+
+
+
+
+
+
