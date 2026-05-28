07e81b7f172580cbe54977b947b6631451d08a08
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-05-29 00:30:28 +0530

    send only required data to frontend

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,18 +1,26 @@
 import userModel from "../models/userModel.js";
-
+// get user data
 export const getUserData = async (req, res) => {
     try {
         const {userId} = req.body;
-
+        
         const user = await userModel.findById(userId);
 
         if(!user){
             res.json({success: false, message: 'User Not Found', data: user})
         }
-
-        res.json({success: true, message: 'User Data Fetched Successfully', data: user});
-
+        // send only required data to frontend
+        res.json({
+            success: true,
+            UserData:{
+                name: user.name,
+                isAccountVerified: user.isAccountVerified,
+            }
+        });
+        // we can also send email and other data if needed, but for now we are sending only name and isAccountVerified
     }catch (error) {
         res.json({success: false, message: error.message});
     }
 }
+
+
