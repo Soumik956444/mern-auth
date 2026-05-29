923691c0ab5dba8df93e7a7d783cdc8d47ddaafb
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-05-29 21:03:22 +0530

    Update userRoutes.js

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,8 +1,9 @@
 import express from 'express';
+import userAuth from '../middleware/userAuth.js';
 
 const userRouter = express.Router();
 
 // Define your user-related routes here
-userRouter.get('/', (req, res) => {
-    
+userRouter.get('/data', userAuth, (req, res) => {
+
 });
