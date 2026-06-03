c346b3c1b76416a1b01a2eb047328ffef35541d6
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-06-03 22:43:27 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Routes, Route} from 'react-router-dom'
+import Home from './components/Home'
 
 const App = () => {
   return (
     <div>
-      
+      <Routes>
+        <Routes path='/' element={<Home/>}/>
+      </Routes>
     </div>
   )
 }
 
 export default App
+// video will resumr at 2hours 41minutes and 7 seconds.
