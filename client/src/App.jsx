59283ad491c63d2b3fb8b2a7c0b8d6c33efb44d3
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-06-03 22:59:46 +0530

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import { Routes, Route} from 'react-router-dom'
-import Home from './components/Home'
+import Home from './components/Home.jsx'
+import login from './components/Login'
+import EmailVerify from './components/EmailVerify'
+import ResetPPassword from './components/ResetPassword'
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Routes path='/' element={<Home/>}/>
+        <Routes path='/login' element={<Login/>}/>
+        <Routes path='/email-verify' element={<EmailVerify/>}/>
+        <Routes path='/reset-password' element={<ResetPassword/>}/>
       </Routes>
     </div>
   )
