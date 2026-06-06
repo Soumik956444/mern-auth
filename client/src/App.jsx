05227fbd02bb6516237da9d07d63bc94d3e80145
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-06-06 23:10:53 +0530

    still

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,35 +1,65 @@
-import React from "react"
-import { BrowserRouter, Routes, Route} from "react-router-dom"
-import Home from "./pages/Home"
-import login from "./pages/Login"
-import EmailVerify from "./pages/EmailVerify"
-import ResetPassword from "./pages/ResetPassword"
+// import React from "react"
+// import { BrowserRouter, Routes, Route} from "react-router-dom"
+// import Home from "./pages/Home"
+// import Login from "./pages/Login"
+// import EmailVerify from "./pages/EmailVerify"
+// import ResetPassword from "./pages/ResetPassword"
+
+// const App = () => {
+//   return (
+//     <BrowserRouter>
+//       <div>
+//         <Routes>
+//           <Route path="./" element={<Home/>}/>
+//           <Route path="./login" element={<Login/>}/>
+//           <Route path="./email-verify" element={<EmailVerify/>}/>
+//           <Route path="./reset-password" element={<ResetPassword/>}/>
+//         </Routes>
+//       </div>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+
+
+
+
+
+
+
+
+
+
+// // facing routing issue     Date: 4th June 2026
+// // video will resume at 2 hours and 40 minutes in the video.
+
+
+
+
+
+
+
+
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import EmailVerify from "./pages/EmailVerify";
+import ResetPassword from "./pages/ResetPassword";
 
 const App = () => {
   return (
     <BrowserRouter>
-      <div>
-        <Routes>
-          <Route path="./" element={<Home/>}/>
-          <Route path="./login" element={<Login/>}/>
-          <Route path="./email-verify" element={<EmailVerify/>}/>
-          <Route path="./reset-password" element={<ResetPassword/>}/>
-        </Routes>
-      </div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
     </BrowserRouter>
-  )
-}
+  );
+};
 
-export default App
-
-
-
-
-
-
-
-
-
-
-// facing routing issue     Date: 4th June 2026
-// video will resume at 2 hours and 40 minutes in the video.
+export default App;
