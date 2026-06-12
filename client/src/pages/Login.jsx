7197982ec21f4ec71ea09d441427ae9556f1d7e5
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-06-12 08:45:42 +0530

    right now creating the login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
+import { useState } from 'react'
 
 const Login = () => {
+
+
+    const [state, setState] = useState('Sign up')
+
   return (
     <div>
-      <h1>Login Page</h1>
+      <img src={assets.login_img} alt="Login"  className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
+      <div>
+        <h2>{state === 'Sign up' ? 'Create your account' : 'Login to your account!'}</h2>
+        <p>{state === 'Sign up' ? 'Create your account' : 'Login to your account!'}</p>
+      </div>
     </div>
   )
 }
+
 
 export default Login
