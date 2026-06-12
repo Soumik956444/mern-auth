3db4aa9e2fa0345b22a39e861eb81d6d078d899c
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-06-12 20:21:59 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { assets } from '../assets/assets'
 import { useState } from 'react'
 
 const Login = () => {
@@ -7,15 +8,28 @@ const Login = () => {
     const [state, setState] = useState('Sign up')
 
   return (
-    <div>
-      <img src={assets.login_img} alt="Login"  className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
+    <div className='flex flex-col items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
+      <img src={assets.logo} alt="Login"  className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
       <div>
-        <h2>{state === 'Sign up' ? 'Create your account' : 'Login to your account!'}</h2>
+        <h2>{state === 'Sign up' ? 'Create account' : 'Login'}</h2>
         <p>{state === 'Sign up' ? 'Create your account' : 'Login to your account!'}</p>
+
+
+
+        <form>
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+            <img src={assets.person_icon} alt="image"/>
+            <input type="text" placeholder="Full Name" required/>
+          </div>
+        </form>
+
+
+
       </div>
     </div>
   )
 }
+
 
 
 export default Login
