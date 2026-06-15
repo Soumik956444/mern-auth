9102acc19d1f6a9fae92a584c5449407ab7e935f
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-06-15 19:34:16 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -9,16 +9,22 @@ const Login = () => {
 
   return (
     <div className='flex flex-col items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
+
       <img src={assets.logo} alt="Login"  className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
+
       <div className=' bg-state-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
-        <h2>{state === 'Sign up' ? 'Create account' : 'Login'}</h2>
-        <p>{state === 'Sign up' ? 'Create your account' : 'Login to your account!'}</p>
+
+        <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign up' ? 'Create account' : 'Login'}</h2>
+
+        <p className='text-center text-sm mb-6'>{state === 'Sign up' ? 'Create your account' : 'Login to your account!'}</p>
 
 
 
         <form>
           <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+
             <img src={assets.person_icon} alt="image"/>
+
             <input type="text" placeholder="Full Name" required/>
           </div>
         </form>
