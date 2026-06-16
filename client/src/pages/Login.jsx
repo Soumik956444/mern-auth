1ff7ce17125d9926c72d308821447cc9778ba3d6
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-06-16 09:46:05 +0530

    Update Login.jsx

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,11 +8,11 @@ const Login = () => {
     const [state, setState] = useState('Sign up')
 
   return (
-    <div className='flex flex-col items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
+    <div className='flex flex-col items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-300 to-purple-400'>
 
       <img src={assets.logo} alt="Login"  className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
 
-      <div className=' bg-state-900 p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
+      <div className=' bg-[#333A5C] p-10 rounded-lg shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
 
         <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign up' ? 'Create account' : 'Login'}</h2>
 
@@ -21,7 +21,7 @@ const Login = () => {
 
 
         <form>
-          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-state-900/50'>
 
             <img src={assets.person_icon} alt="image"/>
 
