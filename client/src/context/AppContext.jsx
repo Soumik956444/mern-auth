1e172874156d9f2e7edcf7739846abcb8ae2037f
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-06-22 22:51:11 +0530

    Update AppContext.jsx

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -19,3 +19,25 @@ export const AppContextProvider = (props)=>{
     )
 
 } 
+
+
+
+
+const AppContextProvider = (props) => {
+
+    const [user, setUser] = useState(null);
+
+    const value = {
+        user,
+        setUser
+    }
+
+    return (
+        <AppContext.Provider value={value}>
+            {props.children}
+        </AppContext.Provider>
+    )
+
+}
+
+export default AppContextProvider;
