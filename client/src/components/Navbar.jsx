68eae332ff97adffc127b88be26dda26b54db38f
Soumik Ghosh <144414867+Soumik956444@users.noreply.github.com>
2026-06-29 22:49:53 +0530

    29.06.2026

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,17 +1,13 @@
 import React from 'react'
 import { assets } from '../assets/assets'
-import { useNavigate } from 'react-router-dom'
 
 const Navbar = () => {
-
-    const navigate = useNavigate()
-
   return (
-    <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absolute top-0'>
+    <div className='w-full flex justify-between items-center p-4 sm:p-6 sm:px-24 absoulte top-0'>
 
       <img src={assets.logo} alt="Logo" className='w-28 sm:w-32'/>
 
-      <button onClick={() => navigate('/login')} className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-200 transition-all'> login <img src={assets.arrow_icon} alt="" /></button>
+      <button className='flex items-center gap-2 border border-gray-500 rounded-full px-6 py-2 text-gray-800 hover:bg-gray-100 transition-all'> login <img src={assets.arrow_icon} alt="" /></button>
     </div>
   )
 }
@@ -22,3 +18,4 @@ export default Navbar
 
 
 
+// video will resume at 2 hours and 49 minutes in the video.
